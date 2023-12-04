b57cc5f3cc74e51c4c3a086271129c65e908e2dc
deedee213 <133877095+dedoyin21@users.noreply.github.com>
2023-12-04 12:38:18 +0100

    learning python structure

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <Python.h>
 /**
- * print_python_list_info - prints python list
+ * print_python_list_info - print out python list
  * @p: a pointer to a pyobj
- * Return: is a void func
+ * Return: is a void function
  */
 void print_python_list_info(PyObject *p)
 {
